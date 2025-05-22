089368db888f98e41695365ff9614226da7bf5c2
Leonardo Rodrigues <leo_rodrigues1999@hotmail.com>
2025-05-22 09:58:09 -0300

    feat(cert-manager): add cert-manager helm and configuration

--- terraform/1-argocd.tf
+++ terraform/1-argocd.tf
@@ -1,10 +1,11 @@
 resource "helm_release" "argocd" {
-  name = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart = "argo-cd"
-  namespace = "argocd"
+  name             = "argocd"
+  namespace        = "argocd"
   create_namespace = true
-  version = "7.8.28"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "7.8.28"
 
   values = [file("values/argocd.yaml")]
 }
--- terraform/2-image-updater.tf
+++ terraform/2-image-updater.tf
@@ -1,11 +1,11 @@
 resource "helm_release" "updater" {
-  name = "updater"
-
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argocd-image-updater"
+  name             = "updater"
   namespace        = "argocd"
   create_namespace = true
-  version          = "0.12.1"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-image-updater"
+  version    = "0.12.1"
 
   values = [file("values/image-updater.yaml")]
 }
